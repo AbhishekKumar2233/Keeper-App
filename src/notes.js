0092323554ed96eb02487a4d75be1388be5daf15
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-17 18:55:45 +0530

    custum data

---
+++ src/notes.js
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    Heading:"JavaScript",
+    Detail:"JavaScript is the world's most popular programming language. JavaScript is the programming language of the Web. JavaScript is easy to learn."
+  },
+  {
+    Heading:"Python",
+    Detail:"Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant.."
+  },
+  {
+    Heading:"Java",
+    Detail:"Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible."
+  },
+  {
+    Heading:"C Language",
+    Detail:"C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system. By design, C provides constructs that map efficiently to typical machine instructions."
+  }
+]
+
+export default notes;
