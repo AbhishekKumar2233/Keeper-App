bc64f78e7321c56739eb6e400ef183993878c1eb
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-17 19:07:33 +0530

    fetch data using map function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Footer from "./components/Footer";
 import Note from "./components/Note";
 import notes from "./notes";
 
+function createNotes(notes) {
+  return <Note heading={notes.Heading} detail={notes.Detail} />;
+}
+
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Note heading={notes[0].Heading} detail={notes[0].Detail} />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
